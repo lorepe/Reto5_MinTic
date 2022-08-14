288bdd859fd5bb524ef9bf33cf4acba068ccc12d
lorepe <loreperez.2003@hotmail.com>
2022-08-13 21:02:36 -0500

    creation project

---
+++ Reto5/src/main/java/com/mycompany/reto5/controlador/Reto5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto5.controlador;
+
+
+import com.mycompany.reto5.vista.frmPrincipal;
+import java.sql.*;
+
+/**
+ *
+ * @author lorep
+ */
+public class Reto5 {
+    public static void main(String[] args) {
+        //Conexion cc = new Conexion();
+        //Connection cn = cc.conectar();
+        
+        new frmPrincipal().setVisible(true);
+        
+    }
+}
